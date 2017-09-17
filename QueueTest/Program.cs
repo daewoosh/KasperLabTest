7dbfd3fbdd58bd36dee16fbcae925436fb6be822
Хельга <Minchenkova@rus-telecom.ru>
2017-09-17 13:37:28 +0300

    first commit

---
+++ QueueTest/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace QueueTest
+{
+	class Program
+	{
+		static void Main()
+		{
+			var q = new TestQueue<int>();
+			List<int> poped = new List<int>();
+
+			Thread popThread = new Thread(() => poped.Add(q.Pop()));
+			Thread popThread1 = new Thread(() => poped.Add(q.Pop()));
+			Thread popThread2 = new Thread(() => poped.Add(q.Pop()));
+
+			q.Push(8);
+
+			
+			
+
+			Thread pushThread = new Thread(() => q.Push(12));
+
+			q.Push(13);
+			popThread2.Start();
+			popThread.Start();
+			popThread1.Start();
+			
+
+			pushThread.Start();
+			Thread.Sleep(1000);
+			foreach (var item in poped)
+			{
+				Console.WriteLine(item);
+			}
+			
+			Console.ReadLine();
+		}
+
+
+	}
+}
---
+++ QueueTest/TestQueue.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace QueueTest
+{
+	public interface ITestQueue<T>
+	{
+		void Push(T element);
+
+		T Pop();
+	}
+
+
+	public class TestQueue<T> : ITestQueue<T>
+	{
+		private Queue<T> _queueInternal;
+
+		private volatile bool _hasElement;
+
+		private object _lockForRead = new object();
+		private object _lockForWrite = new object();
+
+		public TestQueue()
+		{
+			_queueInternal = new Queue<T>();
+			_hasElement = false;
+		}
+
+		public void Push(T element)
+		{
+			lock (_lockForWrite)
+			{
+				_queueInternal.Enqueue(element);
+				_hasElement = true;
+			}
+
+		}
+
+		public T Pop()
+		{
+			lock (_lockForRead)
+			{
+				while (!_hasElement)
+				{ }
+				var res = _queueInternal.Dequeue();
+				_hasElement = _queueInternal.Count > 0;
+				return res;
+			}
+		}
+
+	}
+}

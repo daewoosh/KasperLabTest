e37f9e8816663a46294b6c8ec1abbe7eacd30ee1
Хельга <Minchenkova@rus-telecom.ru>
2017-09-17 19:05:15 +0300

    добавил проект по поиску чисел

---
+++ NumberFinder/NumbersFinder.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.NumbersFinder
+{
+	public class NumbersFinder
+	{
+		public IEnumerable<Tuple<int, int>> FindNumbers(int[] numbers, int sum)
+		{
+			List<Tuple<int, int>> result = new List<Tuple<int, int>>();
+
+			Dictionary<int, int> dict = new Dictionary<int, int>();
+
+			for (int i = 0; i < numbers.Length; i++)
+			{
+				int index = numbers[i];
+				if (dict.ContainsKey(index))
+					dict[index]++;
+				else
+					dict[index] = 1;
+			}
+
+			for (int i = 0; i < numbers.Length; i++)
+			{
+				int dif = sum - numbers[i];
+
+				int count;
+				if (dict.TryGetValue(dif, out count))
+				{
+					if (dif == numbers[i])
+						if (count % 2 == 0 || count > 2)
+						{
+							result.Add(new Tuple<int, int>(numbers[i], dif));
+							dict[dif]--;
+							continue;
+						}
+						else continue;
+					result.Add(new Tuple<int, int>(numbers[i], dif));
+					dict.Remove(numbers[i]);
+				}
+			}
+
+			return result;
+		}
+	}
+}
---
+++ NumberFinder/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.NumbersFinder
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var test = new NumbersFinder();
+			int sum = 8;
+			var res = test.FindNumbers(new int[] { 1, 2, 3, 4, 5, 4, 6, 7, -1, 0, 13, 6, 4, 4 }, sum);
+			foreach (var item in res)
+			{
+				Console.WriteLine("{0}+{1}={2}", item.Item1, item.Item2, sum);
+			}
+			Console.ReadLine();
+		}
+	}
+}
